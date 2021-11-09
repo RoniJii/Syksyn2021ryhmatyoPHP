272d4f75287b39dff489e0a60efcea3a20009518
RoniJii <81521266+RoniJii@users.noreply.github.com>
2021-11-09 15:17:47 +0200

    Käyttäjätarina. Tuoteryhmät

--- index.php
+++ index.php
@@ -2,9 +2,15 @@
 require_once('inc/headers.php');
 require_once('inc/functions.php');
 
-$db = openDb();
-$sql = 'select * from xxxx';
+try {
+    $db = openDb();
+    selectAsJson($db,'select * from category');
+}
+catch (PDOException $pdoex) {
+    returnError($pdoex);
+} 
+/* $sql = 'select * from xxxx';
 $query = $db->query($sql);
 $results = $query->fetchAll(PDO::FETCH_ASSOC);
 header('HTTP/1.1 200 OK');
-print json_encode($results); 
+print json_encode($results);  */
